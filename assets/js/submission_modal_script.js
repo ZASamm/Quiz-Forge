f163b1ed679d3a815342ee86bab96017cbb0061e
Guy Clements <Guy.Clements123@googlemail.com>
2024-10-22 22:51:47 +0000

    Add: Style - Update style for text in modals

--- assets/js/submission_modal_script.js
+++ assets/js/submission_modal_script.js
@@ -1,34 +1,28 @@
 console.log("SUBMISSION Modal script loaded");
 
+// SUBMISSION MODAL 
+document.addEventListener('DOMContentLoaded', handleSubmitModal)
 
-document.addEventListener('DOMContentLoaded', function handleSubmitModal() {
-    const submitModal = document.getElementById("submit-modal");
-    const submitbtn = document.getElementById("questionsBtn");
-    const close = document.getElementsByClassName("close")[1];
+    function handleSubmitModal() {
+        const submitModal = document.getElementById("submit-modal");
+        const submitbtn = document.getElementById("questionsBtn");
+        const close = document.getElementsByClassName("close")[1];
+
+        submitbtn.onclick = function () {
+            submitModal.style.display = "block";
+        };
+
+        close.onclick = function () {
+            submitModal.style.display = "none";
+        };
+
+        window.onclick = function (event) {
+            if (event.target == submitModal) {
+                submitModal.style.display = "none";
+            }
+        };
+    }
+
+
+
     
-    submitbtn.onclick = function() {
-        submitModal.style.display = "block";
-    };
-    
-    close.onclick = function() {
-        submitModal.style.display = "none";
-    };
-
-    window.onclick = function(event) {
-        if (event.target == submitModal) {
-            submitModal.style.display = "none";  
-        }
-    };
-})
-
-
-
-
-
-
-
-
-
-
-
-
