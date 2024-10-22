723fcf754ec6280a7c29e136fea8582f914d00f4
Richard Hallam <richardhallam25@gmail.com>
2024-10-22 11:33:35 +0000

    Add: loadQuestion function to modal_script.js

--- assets/js/modal_script.js
+++ assets/js/modal_script.js
@@ -5,33 +5,50 @@
 
 function handleModal() {
 
-const qModal = document.getElementById("qModal");
-const squares = document.querySelectorAll(".square"); 
-const close = document.getElementsByClassName("close")[0];
+    const qModal = document.getElementById("qModal");
+    const squares = document.querySelectorAll(".square");
+    const close = document.getElementsByClassName("close")[0];
 
-// Add click event to each square
-squares.forEach(square => {
-    square.addEventListener("click", function() {
-        qModal.style.display = "block";
+    // Add click event to each square
+    for (let index = 0; index < squares.length; index++) {
+        const square = squares[index];
+        square.setAttribute("squareIndex", index);
+        square.addEventListener("click", function () {
+            
+            loadQuestion(square.getAttribute("squareIndex"));
+
+            qModal.style.display = "block";
 
 
-        // const squareID = this.id;   - Could set modal content like this
-        // setModalContent(squareID);  - Call function that switches content based on id
+            // const squareID = this.id;   - Could set modal content like this
+            // setModalContent(squareID);  - Call function that switches content based on id
+        });
+    }
 
 
+    /*squares.forEach(square => {
+        square.addEventListener("click", function () {
+            qModal.style.display = "block";
+
+
+            // const squareID = this.id;   - Could set modal content like this
+            // setModalContent(squareID);  - Call function that switches content based on id
+            
+            loadQuestion(square);
+
+        });
+    });*/
+
+    // add click event to close qModal
+    close.addEventListener("click", function () {
+        qModal.style.display = "none";
     });
-});
-
-// add clcik event to close qModal
-close.addEventListener("click", function() {
-    qModal.style.display = "none";
-});
 
 }
 
 // Close modal if clicking anywhere outsde of modal 
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == qModal) {
         qModal.style.display = "none";
     }
@@ -39,3 +56,7 @@ window.onclick = function(event) {
 
 
 document.addEventListener('DOMContentLoaded', handleModal);
+
+function loadQuestion(square) {
+    //alert(`Question ${square} Clicked`);
+}
