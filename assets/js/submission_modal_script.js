00bb10e2cce57ee02bf8622266d6095cc665fc64
Guy Clements <Guy.Clements123@googlemail.com>
2024-10-23 09:24:56 +0000

    Add: Styling to question form

--- assets/js/submission_modal_script.js
+++ assets/js/submission_modal_script.js
@@ -1,26 +1,34 @@
 console.log("SUBMISSION Modal script loaded");
 
+document.addEventListener('DOMContentLoaded', () => {
+    handleSubmitModal();
+    
+
+
+
+});
+
 // SUBMISSION MODAL 
-document.addEventListener('DOMContentLoaded', handleSubmitModal)
+
 
     function handleSubmitModal() {
         const submitModal = document.getElementById("submit-modal");
         const submitbtn = document.getElementById("questionsBtn");
         const close = document.getElementsByClassName("close")[1];
 
-        submitbtn.onclick = function () {
+        submitbtn.addEventListener('click', function () {
             submitModal.style.display = "block";
-        };
+        });
 
-        close.onclick = function () {
+        close.addEventListener('click', function () {
             submitModal.style.display = "none";
-        };
+        });
 
-        window.onclick = function (event) {
-            if (event.target == submitModal) {
+        window.addEventListener('click', function (e) {
+            if (e.target == submitModal) {
                 submitModal.style.display = "none";
             }
-        };
+        });
     }
 
 
